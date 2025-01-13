391c59a0e86267f9a7ff270d2df43abeceae0142
PIERRE DE CASTELBAJAC <decastelbajacpierre@gmail.com>
2025-01-13 09:49:13 +0100

    did step2

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import tileActions from "./modules/tile/tileActions";
 
 const router = express.Router();
 
 /* ************************************************************************* */
-// Define Your API Routes Here
+router.get("/api/tiles", tileActions.browse);
 /* ************************************************************************* */
 
 import boatActions from "./modules/boat/boatActions";
@@ -13,6 +14,7 @@ router.get("/api/boats", boatActions.browse);
 import gameActions from "./modules/game/gameActions";
 
 router.post("/api/games", gameActions.add);
+
 
 /* ************************************************************************* */
 
