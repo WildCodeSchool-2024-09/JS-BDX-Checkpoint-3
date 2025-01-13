a820b93d5b7d63b2cf1f3d732aaa8aeeef939273
Yoann FORTIN <yoann.fortin.dev@gmail.com>
2025-01-13 14:39:44 +0100

    Step 3 done

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -17,6 +17,27 @@ const browse: RequestHandler = async (req, res, next) => {
 
 const edit: RequestHandler = async (req, res, next) => {
   // your code here
+  try {
+    // const id = +req.params.id;
+    const id = Number(req.params.id);
+    const { coord_x, coord_y } = req.body;
+
+    const updatedBoat = {
+      coord_x,
+      coord_y,
+      id,
+    };
+
+    const result = await boatRepository.update(updatedBoat);
+
+    if (result) {
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default {
